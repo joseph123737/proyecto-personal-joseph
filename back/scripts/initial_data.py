6ce0aa5cd546994ec4b48bde5bb8042538c6e28d
josu <josu.oyanguren@gmail.com>
2022-01-20 11:17:24 +0000

    fix initial data import sorting + black as python formatter

--- back/scripts/initial_data.py
+++ back/scripts/initial_data.py
@@ -1,12 +1,16 @@
-import sys
+def main():
+    import sys
 
-sys.path.insert(0, "")
+    sys.path.insert(0, "")
+
+    from src.domain.info import Info, InfoRepository
+
+    database_path = "data/database.db"
+
+    info_repository = InfoRepository(database_path)
+
+    info_repository.save(Info(app_name="f5-seed-app"))
 
 
-from src.domain.info import InfoRepository, Info
-
-database_path = "data/database.db"
-
-info_repository = InfoRepository(database_path)
-
-info_repository.save(Info(app_name="f5-seed-app"))
+if __name__ == '__main__':
+    main()
